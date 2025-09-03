9b9679bde4af90049723c1a02a6e54269621f053
Go-Alpha Ai <hossamelhendawi123@hotmail.com>
2025-09-03 17:57:01 +0300

    script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 
-const API_KEY = "AIzaSyCXD_dTgriN4anDN1nEw_7XapK8-zyToY0"; 
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+const API_KEY = "AIzaSyDxkcOV6h59B_mvXqjrbKfw9Jnyo-VTSZQ"; 
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
  
 const loadDataFromLocalstorage = () => {
